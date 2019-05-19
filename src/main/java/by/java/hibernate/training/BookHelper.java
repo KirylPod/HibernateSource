736842dd4fee9bd3066d4d47fb514884e1f90cae
Kiryl Padhaiski <46563159+KirylPod@users.noreply.github.com>
2019-05-19 10:09:59 +0300

    commit

--- src/main/java/by/java/hibernate/training/BookHelper.java
+++ src/main/java/by/java/hibernate/training/BookHelper.java
@@ -1,6 +1,5 @@
 package by.java.hibernate.training;
 
-import by.java.hibernate.training.HibernateUtil;
 import by.java.hibernate.training.entity.Book;
 import by.java.hibernate.training.entity.Book_;
 import org.hibernate.Session;
@@ -14,13 +13,16 @@ public class BookHelper {
 
     private SessionFactory sessionFactory;
 
-    public BookHelper() { sessionFactory = HibernateUtil.getSessionFactory(); }
+    public BookHelper() {
+
+        sessionFactory = HibernateUtil.getSessionFactory();
+    }
 
     public List<Book> getBookList() {
         // 1 ЭТАП
         Session session = sessionFactory.openSession();  // Открываем сессию для манипуляций с персист объектами
 
-        session.get(Book.class, 1); // получение объета по Id
+//      session.get(Book.class, 1); // получение объета по Id
 
         // 2 ЭТАП ПОДГОТОВКИ ЗАПРОСА
 
@@ -28,19 +30,26 @@ public class BookHelper {
         CriteriaQuery cq = cb.createQuery(Book.class); // при помощи sq будут формироваться запросы
         Root<Book> root = cq.from(Book.class); // основная таблица, корневой entity (в sql запросе - from)
 
-        Selection[] selection = {root.get(Book_.ID), root.get(Book_.NAME), root.get(Book_.TITLE)};
-
-        ParameterExpression<String> nameParam = cb.parameter(String.class, "id");
-
-        cq.select(cb.construct(Book.class, selection)).where(cb.like(root.get(Book_.ID), nameParam));
+        cq.select(root).where(cb.equal(root.get(Book_.id),24) );
 
         // 3 ЭТАП ВЫПОЛНЕНИЯ ЗАПРОСА
 
         Query query = session.createQuery(cq);
-        query.setParameter("id", 24);
-        List<Book> boolList = query.getResultList(); // Создаем коллекцию
+        List<Book> bookList = query.getResultList(); // Создаем коллекцию
+
+
+
+
+
         session.close();
-        return boolList;
+        return bookList;
 
     }
 }
+//    Selection[] selection = {root.get(Book_.id), root.get(Book_.name), root.get(Book_.title)};
+//
+//    ParameterExpression<String> nameParam = cb.parameter(String.class, "name");
+//
+//       cq.select(cb.construct(Book.class, selection)).where(cb.like(root.get(Book_.name), nameParam));
+
+//       query.setParameter("name", "СНЗТ%");
--- src/main/java/by/java/hibernate/training/Start.java
+++ src/main/java/by/java/hibernate/training/Start.java
@@ -3,6 +3,7 @@ package by.java.hibernate.training;
 import by.java.hibernate.training.entity.Book;
 import org.hibernate.Session;
 
+import javax.persistence.criteria.Selection;
 import java.util.Scanner;
 
 public class Start {
@@ -10,12 +11,12 @@ public class Start {
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-//        int operatBook = 24;
+          int operatBook = 24;
 //        String operatCharter = "";
 //        double xOb, nztB, xMin, xMax, nztMin, nztMax;
-
         Session session = HibernateUtil.getSessionFactory().openSession();
-//
+
+
         for (Book book : new BookHelper().getBookList()) {
             System.out.println(
                     book.getId() + " | " + book.getName() + " | " + book.getTitle());
